8d6f30d8328c307f2a33864ce82be30067c5f754
James Chuang <chuang.ja@gmail.com>
2018-12-28 16:56:15 -0500

    Reconnect gene ontology module. Add genic vs intragenic rule and script.

--- Snakefile
+++ Snakefile
@@ -32,7 +32,7 @@ if comparisons_si:
     conditiongroups_si = list(itertools.chain(*[d.keys() for d in config["comparisons"]["spikenorm"]]))
 
 include: "rules/chip-nexus_classify_peaks.smk"
-# include: "rules/chip-nexus_gene_ontology.smk"
+include: "rules/chip-nexus_gene_ontology.smk"
 # include: "rules/chip-nexus_motifs.smk"
 
 onsuccess:
@@ -49,12 +49,15 @@ rule target:
         #categorize DB peaks
         expand(expand("diff_binding/{condition}-v-{control}/libsizenorm/{{category}}/{condition}-v-{control}_{{factor}}-chipnexus-libsizenorm-diffbind-results-{{category}}-{{direction}}.narrowpeak", zip, condition=conditiongroups, control=controlgroups), factor=FACTOR, direction=["all","up","unchanged","down"], category=CATEGORIES),
         expand(expand("diff_binding/{condition}-v-{control}/spikenorm/{{category}}/{condition}-v-{control}_{{factor}}-chipnexus-spikenorm-diffbind-results-{{category}}-{{direction}}.narrowpeak", zip, condition=conditiongroups_si, control=controlgroups_si), factor=FACTOR, direction=["all","up","unchanged","down"], category=CATEGORIES) if comparisons_si else [],
+        #genic vs. intragenic
+        expand(expand("diff_binding/{condition}-v-{control}/libsizenorm/genic_v_intragenic/{condition}-v-{control}_{{factor}}-chipnexus-libsizenorm-genic-v-intragenic.tsv", zip, condition=conditiongroups, control=controlgroups), factor=FACTOR),
+        expand(expand("diff_binding/{condition}-v-{control}/spikenorm/genic_v_intragenic/{condition}-v-{control}_{{factor}}-chipnexus-spikenorm-genic-v-intragenic.tsv", zip, condition=conditiongroups_si, control=controlgroups_si), factor=FACTOR) if comparisons_si else [],
         #DB summary
         expand(expand("diff_binding/{condition}-v-{control}/libsizenorm/{condition}-v-{control}_{{factor}}-chipnexus-libsizenorm-diffbind-mosaic.svg", zip, condition=conditiongroups, control=controlgroups), factor=FACTOR),
         expand(expand("diff_binding/{condition}-v-{control}/spikenorm/{condition}-v-{control}_{{factor}}-chipnexus-spikenorm-diffbind-mosaic.svg", zip, condition=conditiongroups_si, control=controlgroups_si), factor=FACTOR),
-        ##gene ontology
-        #expand(expand("gene_ontology/{condition}-v-{control}/libsizenorm/{{category}}/{condition}-v-{control}_{{factor}}-chipnexus-libsizenorm-{{category}}-{{direction}}-gene-ontology-enriched-all.svg", zip, condition=conditiongroups, control=controlgroups), direction=["up", "down", "unchanged"], category=["genic", "intragenic"], factor=FACTOR) if config["run_gene_ontology"] else [],
-        #expand(expand("gene_ontology/{condition}-v-{control}/spikenorm/{{category}}/{condition}-v-{control}_{{factor}}-chipnexus-spikenorm-{{category}}-{{direction}}-gene-ontology-enriched-all.svg", zip, condition=conditiongroups_si, control=controlgroups_si), direction=["up", "down", "unchanged"], category=["genic", "intragenic"], factor=FACTOR) if config["run_gene_ontology"] and comparisons_si else [],
+        #gene ontology
+        expand(expand("gene_ontology/{condition}-v-{control}/libsizenorm/{{category}}/{condition}-v-{control}_{{factor}}-chipnexus-libsizenorm-{{category}}-{{direction}}-gene-ontology-enriched-all.svg", zip, condition=conditiongroups, control=controlgroups), direction=["up", "down", "unchanged"], category=["genic", "intragenic"], factor=FACTOR) if config["run_gene_ontology"] else [],
+        expand(expand("gene_ontology/{condition}-v-{control}/spikenorm/{{category}}/{condition}-v-{control}_{{factor}}-chipnexus-spikenorm-{{category}}-{{direction}}-gene-ontology-enriched-all.svg", zip, condition=conditiongroups_si, control=controlgroups_si), direction=["up", "down", "unchanged"], category=["genic", "intragenic"], factor=FACTOR) if config["run_gene_ontology"] and comparisons_si else [],
         ##enrichment of known motifs
         #expand(expand("motifs/{condition}-v-{control}/libsizenorm/{{category}}/{{negative}}/{condition}-v-{control}_{{factor}}-chipnexus-libsizenorm-{{category}}-{{direction}}-v-{{negative}}-motif_enrichment.tsv", zip, condition=conditiongroups, control=controlgroups), direction=["up","down"], negative=["unchanged", "random"], category=CATEGORIES, factor=FACTOR) if config["motifs"]["run_motif_analyses"] else [],
         #expand(expand("motifs/{condition}-v-{control}/spikenorm/{{category}}/{{negative}}/{condition}-v-{control}_{{factor}}-chipnexus-spikenorm-{{category}}-{{direction}}-v-{{negative}}-motif_enrichment.tsv", zip, condition=conditiongroups_si, control=controlgroups_si), direction=["up","down"], negative=["unchanged", "random"], category=CATEGORIES, factor=FACTOR) if config["motifs"]["run_motif_analyses"] and comparisons_si else [],
@@ -78,4 +81,18 @@ rule summarise_diffbind_results:
         nexuspipe("envs/tidyverse.yaml")
     script:
         "scripts/plot_diffbind_summary.R"
+
+rule genic_v_intragenic:
+    input:
+        genic = "diff_binding/{condition}-v-{control}/{norm}/genic/{condition}-v-{control}_{factor}-chipnexus-{norm}-diffbind-results-genic-all.tsv",
+        intragenic = "diff_binding/{condition}-v-{control}/{norm}/intragenic/{condition}-v-{control}_{factor}-chipnexus-{norm}-diffbind-results-intragenic-all.tsv",
+    output:
+        tsv = "diff_binding/{condition}-v-{control}/{norm}/genic_v_intragenic/{condition}-v-{control}_{factor}-chipnexus-{norm}-genic-v-intragenic.tsv",
+        lfc_v_lfc = "diff_binding/{condition}-v-{control}/{norm}/genic_v_intragenic/{condition}-v-{control}_{factor}-chipnexus-{norm}-genic-v-intragenic-lfc-v-lfc.svg",
+        lfc_v_expr = "diff_binding/{condition}-v-{control}/{norm}/genic_v_intragenic/{condition}-v-{control}_{factor}-chipnexus-{norm}-genic-v-intragenic-lfc-v-expr.svg",
+        expr_v_expr = "diff_binding/{condition}-v-{control}/{norm}/genic_v_intragenic/{condition}-v-{control}_{factor}-chipnexus-{norm}-genic-v-intragenic-expr-v-expr.svg",
+    conda:
+        nexuspipe("envs/tidyverse.yaml")
+    script:
+        "scripts/tfiib_genic_v_intragenic.R"
 
---
+++ scripts/tfiib_genic_v_intragenic.R
@@ -0,0 +1,146 @@
+library(tidyverse)
+library(broom)
+library(viridis)
+
+main = function(condition, control,
+                genic_path, intra_path,
+                tsv_out, lfc_v_lfc_out, lfc_v_expr_out, expr_v_expr_out,
+                factor){
+    df = read_tsv(genic_path) %>%
+        select(chrom, start, end, peak_name, score, strand,
+               log2_FC=log2_foldchange, lfc_SE, log10_padj,
+               condition_expr, control_expr, peak_summit,
+               feature_name=genic_name) %>%
+        inner_join(read_tsv(intra_path) %>%
+                       select(chrom, start, end, peak_name, score, strand,
+                              log2_FC=log2_foldchange, lfc_SE, log10_padj,
+                              condition_expr, control_expr, peak_summit,
+                              feature_name=orf_name, atg_to_peak_dist),
+                   by = "feature_name", suffix = c("_genic", "_intragenic")) %>%
+        write_tsv(tsv_out)
+
+    lfc_v_lfc = ggplot() +
+        geom_hline(yintercept = 0, size=0.5, color="grey65") +
+        geom_vline(xintercept = 0, size=0.5, color="grey65") +
+        geom_point(data = df,
+                   aes(x=log2_FC_intragenic, y=log2_FC_genic),
+                   size=0.5, alpha=0.6) +
+        # stat_binhex(data = df,
+        #             aes(x=log2_FC_intragenic, y=log2_FC_genic,
+        #                 color=..count..),
+        #             geom="point", binwidth=c(0.1, 0.1),
+        #             size=0.5, alpha=0.6, fill=NA) +
+        geom_smooth(data = df,
+                    aes(x=log2_FC_intragenic, y=log2_FC_genic),
+                    method="lm", size=0.8, color="#114477", alpha=0.4) +
+        geom_label(data = df %>%
+                       do(tidy(lm(.$log2_FC_genic ~
+                                      .$log2_FC_intragenic))) %>%
+                       filter(term != "(Intercept)") %>%
+                       mutate(label = paste("slope =", signif(estimate, 2),
+                                            "\np =",
+                                            scales::scientific(p.value))),
+                   aes(label=label),
+                   x = min(df[["log2_FC_intragenic"]]),
+                   y = max(df[["log2_FC_genic"]]),
+                   hjust=0, vjust=1, alpha=0.6, size=10/72*25.4,
+                   label.size=0) +
+        scale_color_viridis(option="inferno", guide=FALSE) +
+        xlab(bquote(intragenic ~ .(factor) ~ log[2] ~ textstyle(frac(.(condition), .(control))))) +
+        ylab(bquote(atop(genic ~ .(factor), ~ log[2] ~ textstyle(frac(.(condition), .(control)))))) +
+        theme_light() +
+        theme(text = element_text(size=12, color="black"),
+              plot.title = element_text(size=12),
+              strip.background = element_blank(),
+              strip.text = element_text(color="black"),
+              axis.text = element_text(color="black"),
+              axis.title.y = element_text(angle=0, hjust=1, vjust=0.5))
+    ggsave(lfc_v_lfc_out, plot=lfc_v_lfc, width=16, height=12, units="cm")
+
+    lfc_v_expr = ggplot() +
+        geom_hline(yintercept = 0, size=0.5, color="grey65") +
+        geom_vline(xintercept = 0, size=0.5, color="grey65") +
+        geom_point(data = df,
+                   aes(x=control_expr_genic+1, y=log2_FC_intragenic),
+                   size=0.5, alpha=0.6) +
+        # stat_binhex(data = df,
+        #             aes(x=control_expr_genic+1, y=log2_FC_intragenic,
+        #                 color=..count..),
+        #             geom="point", binwidth=c(0.05, 0.1),
+        #             size=0.5, alpha=0.6, fill=NA) +
+        geom_smooth(data = df,
+                    aes(x=control_expr_genic+1, y=log2_FC_intragenic),
+                    method="lm", size=0.8, color="#114477", alpha=0.4) +
+        geom_label(data = df %>%
+                       do(tidy(lm(.$log2_FC_intragenic ~
+                                      log10(.$control_expr_genic+1)))) %>%
+                       filter(term != "(Intercept)") %>%
+                       mutate(label = paste("slope =", signif(estimate, 2),
+                                            "\np =",
+                                            scales::scientific(p.value))),
+                   aes(label=label),
+                   x = log10(min(df[["control_expr_genic"]])+1),
+                   y = max(df[["log2_FC_intragenic"]]),
+                   hjust=0, vjust=1, alpha=0.6, size=10/72*25.4,
+                   label.size=0) +
+        scale_color_viridis(option="inferno", guide=FALSE) +
+        scale_x_log10(name = paste("genic", factor, "occupancy in", control)) +
+        ylab(bquote(atop(intragenic ~ .(factor), ~ log[2] ~ textstyle(frac(.(condition), .(control)))))) +
+        theme_light() +
+        theme(text = element_text(size=12, color="black"),
+              plot.title = element_text(size=12),
+              strip.background = element_blank(),
+              strip.text = element_text(color="black"),
+              axis.text = element_text(color="black"),
+              axis.title.y = element_text(angle=0, hjust=1, vjust=0.5))
+    ggsave(lfc_v_expr_out, plot=lfc_v_expr, width=16, height=12, units="cm")
+
+    expr_v_expr = ggplot() +
+        geom_hline(yintercept = 0, size=0.5, color="grey65") +
+        geom_vline(xintercept = 0, size=0.5, color="grey65") +
+        geom_point(data = df,
+                    aes(x=control_expr_genic+1, y=condition_expr_intragenic+1),
+                   size = 0.5, alpha=0.6) +
+        # stat_binhex(data = df,
+        #             aes(x=control_expr_genic+1, y=condition_expr_intragenic+1,
+        #                 color=..count..),
+        #             geom="point", binwidth=c(0.01, 0.01),
+        #             size=0.5, alpha=0.6, fill=NA) +
+        geom_smooth(data = df,
+                    aes(x=control_expr_genic+1, y=condition_expr_intragenic+1),
+                    method="lm", size=0.8, color="#114477", alpha=0.4) +
+        geom_label(data = df %>%
+                       do(tidy(lm(log10(.$condition_expr_intragenic+1) ~
+                                      log10(.$control_expr_genic+1)))) %>%
+                       filter(term != "(Intercept)") %>%
+                       mutate(label = paste("slope =", signif(estimate, 2),
+                                            "\np =",
+                                            scales::scientific(p.value))),
+                   aes(label=label),
+                   x = log10(min(df[["control_expr_genic"]])+1),
+                   y = log10(max(df[["condition_expr_intragenic"]])+1),
+                   hjust=0, vjust=1, alpha=0.6, size=10/72*25.4,
+                   label.size=0) +
+        scale_color_viridis(option="inferno", guide=FALSE) +
+        scale_x_log10(name = paste("genic", factor, "occupancy in", control)) +
+        scale_y_log10(name = bquote(atop(intragenic ~ .(factor) ~ occupancy, "in" ~ .(condition)))) +
+        theme_light() +
+        theme(text = element_text(size=12, color="black"),
+              plot.title = element_text(size=12),
+              strip.background = element_blank(),
+              strip.text = element_text(color="black"),
+              axis.text = element_text(color="black"),
+              axis.title.y = element_text(angle=0, hjust=1, vjust=0.5))
+    ggsave(expr_v_expr_out, plot=expr_v_expr, width=20, height=12, units="cm")
+}
+
+main(condition=snakemake@wildcards[["condition"]],
+     control=snakemake@wildcards[["control"]],
+     genic_path=snakemake@input[["genic"]],
+     intra_path=snakemake@input[["intragenic"]],
+     tsv_out=snakemake@output[["tsv"]],
+     lfc_v_lfc_out=snakemake@output[["lfc_v_lfc"]],
+     lfc_v_expr_out=snakemake@output[["lfc_v_expr"]],
+     expr_v_expr_out=snakemake@output[["expr_v_expr"]],
+     factor = snakemake@wildcards[["factor"]])
+
